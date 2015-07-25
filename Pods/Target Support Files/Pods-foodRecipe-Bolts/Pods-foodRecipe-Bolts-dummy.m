be2fedb43bc8b646a7b8564fae2a4bb834b57350
zilla <zilla@maksse.com>
2015-07-26 00:44:19 +0900

    untracking

--- Pods/Target Support Files/Pods-foodRecipe-Bolts/Pods-foodRecipe-Bolts-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_foodRecipe_Bolts : NSObject
-@end
-@implementation PodsDummy_Pods_foodRecipe_Bolts
-@end
--- Pods/Target Support Files/Pods-foodRecipe-FBSDKShareKit/Pods-foodRecipe-FBSDKShareKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_foodRecipe_FBSDKShareKit : NSObject
-@end
-@implementation PodsDummy_Pods_foodRecipe_FBSDKShareKit
-@end
